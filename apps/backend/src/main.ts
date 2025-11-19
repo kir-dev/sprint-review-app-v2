2f7f61c13761dbc5e512d01a7a340bb8df689adc
Lipták Péter <liptak.peti.98@gmail.com>
2025-11-19 20:48:39 +0100

    feat: configure dotenv for environment variables and revert PostgreSQL version in docker-compose

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,3 +1,6 @@
+import { config } from 'dotenv';
+config();
+
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
