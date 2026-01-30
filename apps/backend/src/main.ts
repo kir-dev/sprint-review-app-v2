dd8f4f48ee44e9037fd4de998db2e25eda63e3bc
Lipták Péter <liptak.peti.98@gmail.com>
2026-01-30 10:36:58 +0100

    fix: Increase max HTTP header size for production and bind NestJS application to all network interfaces.

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -74,7 +74,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   const port = process.env.PORT ?? 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
   logger.log(`Application is running on: http://localhost:${port}`);
   logger.log(
