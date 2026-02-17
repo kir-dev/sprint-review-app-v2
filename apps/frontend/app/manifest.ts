42ffffff64e7bb1607029ad4d33e5a0de8b40c03
Lipták Péter <liptak.peti.98@gmail.com>
2026-02-17 23:38:28 +0100

    feat: Update web app manifest and metadata icons, including Kir-Dev branding.

--- apps/frontend/app/manifest.ts
+++ apps/frontend/app/manifest.ts
@@ -10,18 +10,30 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: '#ffffff',
     theme_color: '#000000',
     icons: [
-    {
-      "src": "/web-app-manifest-192x192.png",
-      "sizes": "192x192",
-      "type": "image/png",
-      "purpose": "maskable"
-    },
-    {
-      "src": "/web-app-manifest-512x512.png",
-      "sizes": "512x512",
-      "type": "image/png",
-      "purpose": "maskable"
-    }
-  ],
+      {
+        src: '/Kir-Dev-Black.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: '/Kir-Dev-Black.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: '/web-app-manifest-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/web-app-manifest-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+    ],
   }
 }
